c1beb3da9aa21ab7c4a9e80af92f8b38d598545f
Maxi-Nebula <madhuravichandranofficial@gmail.com>
2022-10-16 06:35:09 -0700

    Added set of functions to develop the game

--- RPS.js
+++ RPS.js
@@ -1,16 +1,68 @@
+const choiceArray=['rock','paper','scissors'];
 
+
+function beginGame (){
 function getComputerChoice(ccA)
 {
 
-const randomIndex= Math.floor(Math.random()* computerChoiceArray.length);
+const randomIndex= Math.floor(Math.random()* choiceArray.length);
 
 
 const randomItem=ccA[randomIndex];
+//console.log(randomItem);
 return randomItem;
+
+}
+
+function getUserChoice()
+{
+
+    const userChoice= prompt("Please enter your choice");
+    if(choiceArray.includes(userChoice))
+    {   //console.log(userChoice);
+        return userChoice;
+        
+    }
+    else if(userChoice==''){
+
+    console.log("please enter a value");
+    }
+    else{
+
+        console.log('invalid input');
+    }
+    
+}
+
+const computerChoice=getComputerChoice(choiceArray);
+//console.log(computerChoice);
+const userInput=getUserChoice();
+console.log(userInput);
+
+playRound(computerChoice,userInput);
+
+function playRound(cC,uI)
+{
+
+    if(cC==uI || uI==cC)
+    {
+
+        console.log("Its a DRAW!!!");
+    }
+    else if((cC=='rock'&& uI=='scissors')||(cC=='paper' && uI=='rock')||(cC== 'scissors'&& uI=='paper'))
+    {
+
+        console.log(`I WIN!!! ${cC} beats ${uI}`);
+    }
+    else if((cC=='rock'&&uI=='paper')||(cC=='paper'&&uI=='scissors')||(cC=='scissors'&&uI=='rock'))
+    {
+
+        console.log(`YOU WIN!! ${uI} beats ${cC}`);
+    }
+
+}
 
 
 }
 
-const computerChoiceArray=['rock','paper','scissor'];
-const computerChoice=getComputerChoice(computerChoiceArray);
-console.log(computerChoice);
+beginGame();
