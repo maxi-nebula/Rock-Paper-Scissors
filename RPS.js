bbf6cc1fbbe4d8dcc63e25bfa34528812c1bcfc4
Maxi-Nebula <madhuravichandranofficial@gmail.com>
2022-10-16 09:42:03 -0700

    Added the loop function to only play the game for 5 times

--- RPS.js
+++ RPS.js
@@ -1,7 +1,4 @@
 const choiceArray=['rock','paper','scissors'];
-
-
-function beginGame (){
 function getComputerChoice(ccA)
 {
 
@@ -13,7 +10,6 @@ const randomItem=ccA[randomIndex];
 return randomItem;
 
 }
-
 function getUserChoice()
 {
 
@@ -38,35 +34,79 @@ function getUserChoice()
     
 }
 
-const computerChoice=getComputerChoice(choiceArray);
-//console.log(computerChoice);
-const userInput=getUserChoice();
-console.log(userInput);
-
-playRound(computerChoice,userInput);
-
 function playRound(cC,uI)
 {
-
+    
     if(cC==uI || uI==cC)
-    {
+    { 
 
         console.log("Its a DRAW!!!");
+
+      return 0; 
     }
     else if((cC=='rock'&& uI=='scissors')||(cC=='paper' && uI=='rock')||(cC== 'scissors'&& uI=='paper'))
     {
+ 
 
+        
         console.log(`I WIN!!! ${cC} beats ${uI}`);
+        return 1;
+        
     }
     else if((cC=='rock'&&uI=='paper')||(cC=='paper'&&uI=='scissors')||(cC=='scissors'&&uI=='rock'))
     {
 
         console.log(`YOU WIN!! ${uI} beats ${cC}`);
+        return -1;
+        
+        
     }
-
+   
 }
 
+//const winFactor =playRound(computerChoice,userInput);
+//console.log(winFactor);
+
+
+function beginGame()
+{
+    for(i=0;i<5;i++)
+    
+    {
+
+        let cW=0,uW=0, dW=0;
+        const computerChoice=getComputerChoice(choiceArray);
+        const userInput=getUserChoice();
+        console.log(userInput);
+        const winFactor=playRound(computerChoice,userInput);
+
+          function decideWinner(wF)
+          {
+           if(wF==1)
+           {
+
+            cW=cW+1;
+           }else if(wF==-1)
+           {
+
+            uW=uW+1;
+           }
+           else if(wF==0)
+           {
+
+            dW=dW+1;
+           }
+
+
+          }
+  
+          cW>uW ? console.log("GAME OVER!!!!I WIN"): console.log("GAME OVER!!!YOU WIN");
+
+          decideWinner(winFactor);
+
+
+
 
 }
-
+}
 beginGame();
