fefc708a9c453257390f515747ec5096856ece04
Maxi-Nebula <madhuravichandranofficial@gmail.com>
2022-10-16 22:47:30 -0700

    partial o/p reached needs further testing

--- RPS.js
+++ RPS.js
@@ -34,15 +34,28 @@ function getUserChoice()
     
 }
 
-function playRound(cC,uI)
+
+
+
+
+//const winFactor =playRound(computerChoice,userInput);
+//console.log(winFactor);
+ 
+function beginGame()
 {
+
+
+
+    function playRound(cC,uI)
+{
+   
     
     if(cC==uI || uI==cC)
     { 
 
         console.log("Its a DRAW!!!");
 
-      return 0; 
+      return 0;
     }
     else if((cC=='rock'&& uI=='scissors')||(cC=='paper' && uI=='rock')||(cC== 'scissors'&& uI=='paper'))
     {
@@ -63,33 +76,52 @@ function playRound(cC,uI)
     }
     
    
+
+
 }
 
-
-
-//const winFactor =playRound(computerChoice,userInput);
-//console.log(winFactor);
- 
-function beginGame()
-{
+    let cW=0;uW=0;dW=0;
     for(i=0;i<5;i++)
-    
+  
     {
 
-      
+     
         const computerChoice=getComputerChoice(choiceArray);
         const userInput=getUserChoice();
         console.log(userInput);
         const winFactor= playRound(computerChoice,userInput);
-        
+       if (winFactor==1)
+       { cW=cW+1;
 
-       
-             
+        
+       }
+       else if (winFactor==-1)
+       {
+        uW=uW+1;
+        
+       }
+
+
+function decideWinner(C,U)
+{
+if(C>U)
+
+{
+
+    console.log("----------GAME OVER!! I WIN---------------");
+}
+else if(U>C)
+{
+
+    console.log("**************CONGRATULATIONS!!YOU BEAT ME**********");
+}
+}
+                 
 
 }
 
 
-return winFactor;
+decideWinner(cW,uW);
 
 
 }
